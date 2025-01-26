22cf0e96dd245588d961720f81fc91267d88fbc1
mittalpragat <mittalpragat@gmail.com>
2025-01-26 20:21:53 +0530

    tikl

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('hey');
+    res.render('hey');
 })
 
 app.get('/create', async (req, res) => {
